daf69bccd2b29b013b06cc32e7b0fefdeab12140
Ivona Nicetin <ivona.nicetin@gmail.com>
2025-02-14 23:33:59 -0800

    Player movement

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,19 +4,26 @@ using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     public UnityEvent OnSpacePressed = new UnityEvent();
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             OnSpacePressed?.Invoke();
         }
+        Vector2 input = Vector2.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            input += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            input += Vector2.right;
+        }
+        OnMove?.Invoke(input);
     }
+
+    
 }
