194d9fe895cfbc18d89f923f92c438b8f0c039fa
Ivona Nicetin <ivona.nicetin@gmail.com>
2025-02-15 20:04:59 -0800

    Added Scoring system

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private float score = 0;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    private FallTrigger[] pins;
+
+    private void Start()
+    {
+        // Find all active FallTrigger objects
+        pins = FindObjectsOfType<FallTrigger>();
+
+        foreach (FallTrigger pin in pins)
+        {
+            pin.OnPinFall.AddListener(IncrementScore);
+        }
+    }
+
+    private void IncrementScore()
+    {
+        score++;
+        scoreText.text = $"Score: {score}";
+    }
+}
